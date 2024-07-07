78539c5e69ae290024534bc50da152fa2ab8644a
reliq <reliq@reliqarts.com>
2024-07-08 01:00:47 +0200

    initialize invervention imagemanager with gd driver by default

--- src/Service/ImageManager.php
+++ src/Service/ImageManager.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace ReliqArts\GuidedImage\Service;
 
+use Intervention\Image\Exceptions\DriverException;
 use Intervention\Image\ImageManager as InterventionImageManager;
 use Intervention\Image\Interfaces\DecoderInterface;
 use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Interfaces\ImageManagerInterface;
 use ReliqArts\GuidedImage\Contract\ImageManager as ImageManagerContract;
 use RuntimeException;
 
-final readonly class ImageManager implements ImageManagerContract
+final class ImageManager implements ImageManagerContract
 {
-    public function __construct(private InterventionImageManager $manager)
+    /**
+     * @throws DriverException
+     */
+    public function __construct(private ?ImageManagerInterface $manager = null)
     {
+        $this->manager = $manager ?? InterventionImageManager::gd();
     }
 
     /**
